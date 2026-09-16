f0ef119c68bb13af9edf0e88b52427df606d933d
OscarBenois <nelly.kirshina23@gmail.com>
2026-09-17 00:05:54 +0300

    Базовый ввод данных

--- ISIP324_KirshinaNelly/Program.cs
+++ ISIP324_KirshinaNelly/Program.cs
@@ -10,6 +10,22 @@ namespace ISIP324_KirshinaNelly
     {
         static void Main(string[] args)
         {
+            List<string> products = new List<string>();
+            List<double> costs = new List<double>();
+            int numberofoperations = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите количество операций (от 2 до 40)");
+                numberofoperations = Convert.ToInt32(Console.ReadLine());
+                if (numberofoperations >= 2 && numberofoperations <= 40)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка. Введите число операций от 2 до 40.");
+                }
+            }
         }
     }
 }
