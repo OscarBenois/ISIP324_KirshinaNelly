f6edfd7479f3138547e6726957c2531937da0108
OscarBenois <nelly.kirshina23@gmail.com>
2026-09-17 00:10:20 +0300

    запись в списки, ввод трат по шаблону "Название; Цена"

--- ISIP324_KirshinaNelly/Program.cs
+++ ISIP324_KirshinaNelly/Program.cs
@@ -26,6 +26,26 @@ namespace ISIP324_KirshinaNelly
                     Console.WriteLine("Ошибка. Введите число операций от 2 до 40.");
                 }
             }
+            for (int i = 0; i < numberofoperations; i++)
+            {
+                Console.WriteLine("Введите трату в формате: Название услуги или товара; Цена");
+                string input = Console.ReadLine();
+                string[] parts = input.Split(';');
+                if (parts.Length == 2)
+                {
+                    string product = parts[0].Trim();
+                    int cost = Convert.ToInt32(parts[1].Trim());
+                    products.Add(product);
+                    costs.Add(cost);
+                    Console.WriteLine($"Записано: {product} - {cost} рублей");
+                }
+                else
+                {
+                    Console.WriteLine("Исспользуется неверный формат ввода. Попробуйте снова.");
+                    i--;
+                }
+            
+            }
         }
     }
 }
