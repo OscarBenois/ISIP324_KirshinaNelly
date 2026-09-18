01a5b7dadfd9c76e828518026a1677411d5848f1
OscarBenois <nelly.kirshina23@gmail.com>
2026-09-18 12:13:11 +0300

    классы и перечисления описывающие данные

--- ISIP324_KirshinaNelly/Program.cs
+++ ISIP324_KirshinaNelly/Program.cs
@@ -8,8 +8,31 @@ namespace ISIP324_KirshinaNelly
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public enum Category { Напитки, Выпечка, Фастфуд, Хозтовары, Еда }
+
+        public class Product
         {
+            private static int nextCode = 1; 
+
+            public int code;
+            public string name;
+            public double price; 
+            public int quantity;
+            public Category category;
+
+            
+            public bool isThere => quantity > 0;
+
+            public Product(string name, double price, int quantity, Category category)
+            {
+                this.code = nextCode++;
+                this.name = name;
+                this.price = price;
+                this.quantity = quantity;
+                this.category = category;
+            }
+            public void PrintInfo()
+            {
+                Console.WriteLine($"[Код: {code}] {name} | Цена: {price} | Остаток: {quantity} | Категория: {category} | В наличии: {(isThere ? "Да" : "Нет")}");
+            }
         }
-    }
-}
